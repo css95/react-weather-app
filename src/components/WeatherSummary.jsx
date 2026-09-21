f614849abff324d49481a5f6517bb6080d5fd9b9
css95 <steilner.carolin@gmail.com>
2026-09-21 19:28:37 +0200

    Disable click and cursor on 'Your location'

--- src/components/WeatherSummary.jsx
+++ src/components/WeatherSummary.jsx
@@ -6,13 +6,18 @@ function WeatherSummary({ cityName, icon, temperature }) {
     const navigate = useNavigate();
 
     function handleClick() {
+        if (cityName === 'Your location') return;
         navigate(`/city/${cityName.toLowerCase()}`);
     }
 
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.title}>{cityName}</h1>
-            <button className={styles.weatherSummary} onClick={handleClick}>
+            <button 
+                className={styles.weatherSummary} 
+                onClick={handleClick}
+                style = {{ cursor: cityName === 'Your location' ? 'default' : 'pointer' }}
+            >
                 <div className={styles.weatherMain}>
                     <span className={styles.icon}>{icon}</span>
                     <h2 className={styles.temperature}>{temperature}°</h2>
